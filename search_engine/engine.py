1bb57bdccb707b891ebe0ca60d10edf5d3805aa6
Valeriu Vicol <valeriu.vicol@mirriad.com>
2022-05-13 08:58:50 +0200

    refactor engine

--- search_engine/engine.py
+++ search_engine/engine.py
@@ -1,52 +1,49 @@
 from collections import defaultdict
 from typing import DefaultDict
-from search_engine.services.corpus_service import TextCorpusService,AbstractCorpusService
-from search_engine.services.preprocesing_service import RawPreprocesingService,AbstractPreprocesingService
+from search_engine.services.corpus_service import TextCorpusService, AbstractCorpusService
+from search_engine.services.preprocesing_service import RawPreprocesingService, AbstractPreprocesingService
 from loguru import logger
-from typing import DefaultDict, Set
-
+from typing import DefaultDict, Set, List
 
 class Engine:
     """
-        core class of the project which load the corpus, preprocess it, 
-        and store in memory inverted index 
+    Core class of the project which load the corpus, preprocess it, 
+    and store in memory inverted index 
     """
 
-    def __init__(self, corpus_service=TextCorpusService(), preprocessor=RawPreprocesingService()):
-        self._corpus_service:AbstractCorpusService = corpus_service
-        self._preprocessor:AbstractPreprocesingService = preprocessor
-        self._index:DefaultDict[str, set]  = defaultdict(set)
+    def __init__(self, corpus_service: AbstractCorpusService, preprocessor: AbstractPreprocesingService):
+        self.__corpus_service = corpus_service
+        self.__preprocessor = preprocessor
+        self.__indicies: DefaultDict[str, DefaultDict[str, Set]] = defaultdict(lambda: defaultdict(set))
 
         logger.debug('indexing corpus')
-        for name, doc in self._corpus_service.load_corpus():
-            self._preprocessor.process(name, doc, self._index)
+        for name, doc in self.__corpus_service.load_corpus():
+           self.__indicies = self.__preprocessor.process(name, doc, dictionary=self.__indicies)
         logger.debug('indexing finished')
 
-    
     def search(self, word: str) -> dict:
         word = word.lower()
-        if word not in self._index.keys():
+        if word not in self.__indicies:
             return {}
-        return self._colect_entries(word)
+        return self.__collect_entries(word)
 
-    
-    def _colect_entries(self, word: str) -> DefaultDict[str, Set]:
+    def __collect_entries(self, word: str) -> DefaultDict[str, List[str]]:
         """
-            function which will extract the sentence from files based on key word,
-            and will map everything into dictionary
+        Function which will extract the sentence from files based on key word,
+        and will map everything into dictionary
         """
         collecting_dict = defaultdict(list)
-        word_metadata = self._index[word]
+        word_metadata = self.__indicies[word]
 
-        for file, phrase_range in word_metadata:
-
+        for file in word_metadata:
             try:
-                doc = self._corpus_service.load_document(file)
+                doc = self.__corpus_service.load_document(file)
             except FileNotFoundError:
                 logger.error(f'file {file} is missing from corpus')
                 return {}
 
-            starting_index, ending_index = phrase_range
-            collecting_dict[file].append(doc[starting_index:ending_index].strip())
+            for phrase_range in word_metadata[file]:
+                start, end = phrase_range
+                collecting_dict[file].append(doc[start:end].strip())
 
         return collecting_dict
