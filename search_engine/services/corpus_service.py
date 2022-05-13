fcd5a7b0f6d4579ea46f089eea97801097333203
Valeriu Vicol <valeriu.vicol@mirriad.com>
2022-05-13 08:45:44 +0200

    refactor corpus service to use protocol and path lib

--- search_engine/services/corpus_service.py
+++ search_engine/services/corpus_service.py
@@ -1,60 +1,52 @@
 import glob
-from typing import Tuple,Generator
+from typing import Tuple, Generator
 from search_engine.core.exceptions import EmptyCorpusException
 from search_engine.core.config import CORPUS_PATH
 from loguru import logger
-import abc
+from pathlib import Path
+from typing import Protocol
 
 
-class AbstractCorpusService:
+class AbstractCorpusService(Protocol):
     """
-        defines contract for corpus services which should load the data into memory
+    Defines contract for corpus services which should load the data into memory.
     """
 
-    @abc.abstractclassmethod
-    def load_corpus(self)-> Generator[Tuple[str, str], None, None]:
+    def load_corpus(self) -> Generator[Tuple[str, str], None, None]:
         """
-            creates a generetor for files/sql-row/no-sql inside the corpus folder
-            
-            :param root_folder: path to corpus folder
-            :rtype Iterator for file name and file content
+        Creates a generetor for files/sql-row/no-sql corpus
+        :rtype Generator for file name and file content
         """
-        raise NotImplementedError("Call on abstraction method of AbstractCorpusService")
+        pass
 
-
-    @abc.abstractclassmethod
-    def load_document(self,doc_name:str)->str:
+    def load_document(self, doc_name: str) -> str:
         """
-            should return the document by name
+        Should return the document by name
         """
-        raise NotImplementedError("Call on abstraction method of AbstractCorpusService")
+        pass
 
 
 class TextCorpusService(AbstractCorpusService):
     """
-        implementation of AbstractCorpusService which load files/corpus from a folder 
+    Implementation of AbstractCorpusService which load files/corpus from a folder 
     """
 
     def __init__(self, corpus_path: str = CORPUS_PATH):
-        self._corpus_path = corpus_path
-   
+        self.__corpus_path: Path = Path(corpus_path)
 
-    def load_corpus(self) -> Tuple[str, str]:
+    def load_corpus(self) -> Generator[Tuple[str, str], None, None]:
         """
-            list files from folder and preprocess them
+        List files from folder and return name of the file with content of the file
         """
-        root_len = len(self._corpus_path)
-        files_path = glob.glob(f'{self._corpus_path}*.txt')
-        
-        if len(files_path) == 0:
-            raise EmptyCorpusException(f"empty corpus at {self._corpus_path}")
-        
-        for path in files_path:
-            file_name = path[root_len:-4]
-            yield file_name, self.load_document(file_name)
+        files_list = list(self.__corpus_path.glob('*.txt'))
 
-    
-    def load_document(self, name: str) -> str:
-        with open(f'{self._corpus_path}{name}.txt') as file:
+        if not files_list:
+            logger.error('Corpus is missing')
+            raise EmptyCorpusException(str(self.__corpus_path.absolute()))
+
+        for path in files_list:
+            yield path.stem, self.load_document(path.stem)
+
+    def load_document(self, doc_name: str) -> str:
+        with self.__corpus_path.joinpath(f'{doc_name}.txt').open('r') as file:
             return file.read().replace('\n', '')
-            
