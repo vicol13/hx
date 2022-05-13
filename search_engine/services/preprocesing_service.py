d4489e149532a08903b251760abe02e15e3ac335
Valeriu Vicol <valeriu.vicol@mirriad.com>
2022-05-13 09:19:31 +0200

    move to new line output of function

--- search_engine/services/preprocesing_service.py
+++ search_engine/services/preprocesing_service.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
+from typing import DefaultDict, Set, Protocol
 from search_engine.core.config import STOP_WORDS
-from typing import DefaultDict, Set
-from typing import Protocol
 
 
 class AbstractPreprocesingService(Protocol):
@@ -9,33 +8,32 @@ class AbstractPreprocesingService(Protocol):
     Defines the contract for preprocesing services which have to return an inverted index
     """
 
-    def process(self, doc_name: str, doc: str, dictionary: DefaultDict[str, DefaultDict[str, Set]]) -> DefaultDict[str, DefaultDict[str, Set]]:
+    def process(self, doc_name: str, doc: str, dictionary: DefaultDict[str, DefaultDict[str, Set]]) \
+            -> DefaultDict[str, DefaultDict[str, Set]]:
         """
         :rparam doc_name: name of the document
-        :rparam doc:   input string which will be tokenized 
+        :rparam doc:   input string which will be tokenized
         :rparam dictionary: represent a default map in case when we want to get new index for current document
-                            or to update and existing index with this document 
-            
-            :rtype: dictionary of next format:
-            {
-                '<word>': {
-                    '<document_name> : {(<start>,<end>),(<start>,<end>)},
-                    '<document2_name> : {(<start>,<end>),(<start>,<end>)}
-                }
-                
-            }
-            indexes represent the begin and the end of the sentence where word had been found
+                            or to update and existing index with this document
+        :rtype: dictionary of next format:
+        {
+            '<word>': {
+                '<document_name> : {(<start>,<end>),(<start>,<end>)},
+                '<document2_name> : {(<start>,<end>),(<start>,<end>)}
+            } 
+        }
+        indexes represent the begin and the end of the sentence where word had been found
         """
-        pass
 
 
 class RawPreprocesingService(AbstractPreprocesingService):
     """
-    Implementation AbstractPreprocesingService which does't perform any special preprocesing steps 
-    on corpus it just index each word 
+    Implementation AbstractPreprocesingService which does't perform any special preprocesing steps
+    on corpus it just index each word
     """
 
-    def process(self, doc_name: str, doc: str, dictionary: DefaultDict[str, DefaultDict[str, Set]] = None) -> DefaultDict[str, DefaultDict[str, Set]]:
+    def process(self, doc_name: str, doc: str, dictionary: DefaultDict[str, DefaultDict[str, Set]] = None)\
+            -> DefaultDict[str, DefaultDict[str, Set]]:
         """
         Parse the document remove stop words and return inverted index of document
         """
